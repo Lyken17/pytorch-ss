Fix overflow bug
Avoid overflow in torch.ByteTensor

loss.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, division, absolute_import
+
 import torch
 import torch.nn.functional as F
 
@@ -7,12 +9,14 @@ def CrossEntropy2d(input, target, weight=None, size_average=False):
 
     input = input.transpose(1, 2).transpose(2, 3).contiguous()
     input = input[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0].view(-1, c)
-
+    
     target_mask = target >= 0
     target = target[target_mask]
     #loss = F.nll_loss(F.log_softmax(input), target, weight=weight, size_average=False)
     loss = F.cross_entropy(input, target, weight=weight, size_average=False)
+    
     if size_average:
-        loss /= target_mask.sum().data[0]
+        loss /= target_mask.data.sum()
 
     return loss
+